cf9866f6218a9e15e03190d5069900e0fe23e23b
oleg <perchykoleg@gmail.com>
2019-09-26 18:52:03 -0500

    Updates

---
+++ precache-manifest.311208bbe161517afcd07072e7388c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689aaa84f52b9ef27856d1ec9547a9bb",
+    "url": "/chonk-react/index.html"
+  },
+  {
+    "revision": "81c682743dd74baf3d06",
+    "url": "/chonk-react/static/css/main.894a8dcd.chunk.css"
+  },
+  {
+    "revision": "e6434658dcaa5f9cf440",
+    "url": "/chonk-react/static/js/2.a4c8419d.chunk.js"
+  },
+  {
+    "revision": "81c682743dd74baf3d06",
+    "url": "/chonk-react/static/js/main.60eaf015.chunk.js"
+  },
+  {
+    "revision": "ba3c3324185561faec8c",
+    "url": "/chonk-react/static/js/runtime~main.ecb9cd9e.js"
+  },
+  {
+    "revision": "fdbf1c8b160c4b3ba2d8bdf836b9587d",
+    "url": "/chonk-react/static/media/ramen.fdbf1c8b.jpg"
+  }
+]);
